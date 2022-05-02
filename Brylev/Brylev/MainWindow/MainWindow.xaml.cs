1c82cd408a1ab0813de642ed671764ef9c336b57
OldMan <mikovich.mike@gmail.com>
2022-05-02 20:49:51 +0300

    Hotfix

--- Brylev/Brylev/MainWindow/MainWindow.xaml.cs
+++ Brylev/Brylev/MainWindow/MainWindow.xaml.cs
@@ -10,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Brylev
+namespace Brylev.MainWindow
 {
 	/// <summary>
 	/// Логика взаимодействия для MainWindow.xaml
