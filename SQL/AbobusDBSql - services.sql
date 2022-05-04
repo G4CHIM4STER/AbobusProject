dd3c5cb5909545e52a8c90a08c5a5bd770def574
G4CHIM4STER <veksler.leonid@yandex.ru>
2022-05-05 01:36:31 +0300

    Хуета с базами данных запутались и перепутались

--- SQL/AbobusDBSql - services.sql
+++ SQL/AbobusDBSql - services.sql
@@ -1,7 +1,13 @@
-DROP DATABASE AbobusDB;
+CREATE TABLE AbobusDB.dbo.ApplicationEquipment ( 
+	id_appliactionequipment int    IDENTITY(1,1)  NOT NULL,
+	CONSTRAINT pk_ApplicationEquipment PRIMARY KEY  ( id_appliactionequipment ) 
+ );
 GO
 
-CREATE DATABASE AbobusDB;
+CREATE TABLE AbobusDB.dbo.ApplicationServices ( 
+	id_applicationservice int    IDENTITY(1,1)  NOT NULL,
+	CONSTRAINT pk_ApplicationService PRIMARY KEY  ( id_applicationservice ) 
+ );
 GO
 
 CREATE TABLE AbobusDB.dbo.ApplicationTypes ( 
@@ -59,6 +65,23 @@ CREATE TABLE AbobusDB.dbo.Rooms (
  );
 GO
 
+CREATE TABLE AbobusDB.dbo.Services ( 
+	id_service           int    IDENTITY(1,1)  NOT NULL,
+	name                 varchar(100)      NOT NULL,
+	description          varchar(200)      NOT NULL,
+	cost                 int      NOT NULL,
+	CONSTRAINT pk_Services PRIMARY KEY  ( id_service ) 
+ );
+GO
+
+CREATE TABLE AbobusDB.dbo.ServicesList ( 
+	id_serviceslist      int    IDENTITY(1,1)  NOT NULL,
+	id_service           int      NOT NULL,
+	id_application_service int      NOT NULL,
+	CONSTRAINT pk_ServicesList PRIMARY KEY  ( id_serviceslist ) 
+ );
+GO
+
 CREATE TABLE AbobusDB.dbo.Statuses ( 
 	id_status            int    IDENTITY(1,1)  NOT NULL,
 	name                 varchar(100)      NOT NULL,
@@ -107,7 +130,16 @@ CREATE TABLE AbobusDB.dbo.Equipment (
 	type                 varchar(100)      NOT NULL,
 	serial_number        nvarchar(20)      NOT NULL,
 	description          varchar(200)      NULL,
+	cost                 int      NOT NULL,
 	CONSTRAINT pk_Equipment PRIMARY KEY  ( id_equip ) 
+ );
+GO
+
+CREATE TABLE AbobusDB.dbo.EquipmentList ( 
+	id_equipmentlist     int    IDENTITY(1,1)  NOT NULL,
+	id_equip             int      NOT NULL,
+	id_applicationequipment int      NOT NULL,
+	CONSTRAINT pk_EquipmentList PRIMARY KEY  ( id_equipmentlist ) 
  );
 GO
 
@@ -120,6 +152,8 @@ CREATE TABLE AbobusDB.dbo.Applications (
 	id_agreement         int      NOT NULL,
 	id_docs              int      NOT NULL,
 	id_employee          int      NOT NULL,
+	id_applicationservice int      NOT NULL,
+	id_applicationequipment int      NOT NULL,
 	date                 datetime      NULL,
 	description          varchar(200)      NULL,
 	CONSTRAINT pk_Application PRIMARY KEY  ( id_application ) 
@@ -138,7 +172,7 @@ GO
 ALTER TABLE AbobusDB.dbo.Applications ADD CONSTRAINT fk_Application_Status FOREIGN KEY ( id_status ) REFERENCES AbobusDB.dbo.Statuses( id_status );
 GO
 
-ALTER TABLE AbobusDB.dbo.Applications ADD CONSTRAINT fk_Application_ApplicationTypes FOREIGN KEY ( id_aplicationtype ) REFERENCES AbobusDB.dbo.ApplicationTypes( id_applicationtype );
+ALTER TABLE AbobusDB.dbo.Applications ADD CONSTRAINT fk_Application_ApplicationType FOREIGN KEY ( id_aplicationtype ) REFERENCES AbobusDB.dbo.ApplicationTypes( id_applicationtype );
 GO
 
 ALTER TABLE AbobusDB.dbo.Applications ADD CONSTRAINT fk_Application_Equipment FOREIGN KEY ( id_equip ) REFERENCES AbobusDB.dbo.Equipment( id_equip );
@@ -151,6 +185,12 @@ ALTER TABLE AbobusDB.dbo.Applications ADD CONSTRAINT fk_Application_Docs FOREIGN
 GO
 
 ALTER TABLE AbobusDB.dbo.Applications ADD CONSTRAINT fk_Application_Employee FOREIGN KEY ( id_employee ) REFERENCES AbobusDB.dbo.Employees( id_employee );
+GO
+
+ALTER TABLE AbobusDB.dbo.Applications ADD CONSTRAINT fk_Application_ApplicationService FOREIGN KEY ( id_applicationservice ) REFERENCES AbobusDB.dbo.ApplicationServices( id_applicationservice );
+GO
+
+ALTER TABLE AbobusDB.dbo.Applications ADD CONSTRAINT fk_Application_ApplicationEquipment FOREIGN KEY ( id_applicationequipment ) REFERENCES AbobusDB.dbo.ApplicationEquipment( id_appliactionequipment );
 GO
 
 ALTER TABLE AbobusDB.dbo.Clients ADD CONSTRAINT fk_Client_Room FOREIGN KEY ( id_room ) REFERENCES AbobusDB.dbo.Rooms( id_room );
@@ -174,3 +214,90 @@ GO
 ALTER TABLE AbobusDB.dbo.Equipment ADD CONSTRAINT fk_Equipment_Agreement FOREIGN KEY ( id_agreement ) REFERENCES AbobusDB.dbo.Agreements( id_agreement );
 GO
 
+ALTER TABLE AbobusDB.dbo.EquipmentList ADD CONSTRAINT fk_EquipmentList_ApplicationEquipment FOREIGN KEY ( id_applicationequipment ) REFERENCES AbobusDB.dbo.ApplicationEquipment( id_appliactionequipment );
+GO
+
+ALTER TABLE AbobusDB.dbo.EquipmentList ADD CONSTRAINT fk_EquipmentList_Equipment FOREIGN KEY ( id_equip ) REFERENCES AbobusDB.dbo.Equipment( id_equip );
+GO
+
+ALTER TABLE AbobusDB.dbo.ServicesList ADD CONSTRAINT fk_ServicesList_ApplicationService FOREIGN KEY ( id_application_service ) REFERENCES AbobusDB.dbo.ApplicationServices( id_applicationservice );
+GO
+
+ALTER TABLE AbobusDB.dbo.ServicesList ADD CONSTRAINT fk_ServicesList_Services FOREIGN KEY ( id_service ) REFERENCES AbobusDB.dbo.Services( id_service );
+GO
+
+exec [AbobusDB].sys.sp_addextendedproperty  @name=N'MS_Description', @value=N'Площадь' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Rooms', @level2type=N'COLUMN',@level2name=N'space';
+GO
+
+INSERT INTO Clients VALUES
+
+(1, 'Андрей Андреевич Брылев', 'brylev@aboba.com', '7(29)950-2577'),
+(2, 'Дмитрий Алексеевич Иванов', 'ivanov@russia.com', '7(44)098-8354'),
+(3, 'Семен Семенович Спермагорский', 'spermach@slave.ru', '7(835)385-2081'),
+(4, 'Толмас Сергеев Русланович', 'kakaoymozhnosdelatvyvod@nikakogo.ru', '7(7670)485-8339'),
+(5, 'Михаил Алексеевич Пушка', 'rukabazuka@gmail.eu', '7(28)731-6773')
+
+INSERT INTO Roles VALUES
+
+('Тут что-то'),
+('Тут тоже'),
+('И тут'),
+('Тут тоже, наверное'),
+('Шахтер')
+
+INSERT INSERT Regions VALUES
+
+('Астраханский'),
+('Марсовский'),
+('Устричный'),
+('Бедный'),
+('Абобный')
+
+INSERT INTO Positions VALUES
+
+('Менеджер по продажам'),
+('Менеджер по лажам'),
+('Работяга'),
+('Летяга'),
+('Админитрахор'),
+
+INSERT INTO Employees VALUES
+
+(1, 1, 1, 'Федор Борисович Борисов','7(455)483-4152' , 'borisborisov@boris.bor'),
+(2, 3, 1, 'Артур Антонов Пирожков','7(3689)525-1187' , 'artur@ch.ik'),
+(4, 2, 3, 'Владимир Владимирович Гутлер','7(455)483-4152' , 'rossiyarossiya@putler.vor'),
+(5, 1, 4, 'Федор Михайлович Кунжут','7(454)316-7418' , 'echpochmak@maka.net'),
+(3, 4, 2, 'Леонид Семенович Борг','7(497)777-8240' , 'borg@borg.borg'),
+
+INSERT INTO Statuses VALUES
+
+('На рассмотрении'),
+('Выполняется'),
+('Заморожен'),
+('Отложен'),
+('Отменен')
+
+INSERT INTO Rooms VALUES
+
+('Туалет',0 ,228),
+('Уборная',4 ,1488),
+('Санузел',2 ,322),
+('Ванная комната', 3, 1984)
+
+INSERT INTO Applications VALUES
+
+(1,1 ,3 ,3 ,1 ,2 ,3 ,'2016-12-25T15:32:06.427' ,"Надо сдлеать !" ),
+(2, 4,4 , 1, 1, 4,1 , '2025-12-25T15:32:06.427', "Не надо сделать" ),
+(3,3 ,2 ,4 ,1 ,2 ,2 ,'2019-12-25T15:32:06.427' , "Точно не надо !"),
+(4, 2,1 ,2 , 1,1 ,3 , '2005-12-25T15:32:06.427', "№Вставить описание№"),
+(2,5 ,2 , 1, 1,4 ,4 ,'2017-12-25T15:32:06.427' , "А шо это ты тут забыл ?")
+
+INSERT INTO ApplicationTypes VALUES
+
+('Починка', 'Опять работать'),
+('Поломка', 'Ломать - не строить'),
+('Починко', 'Автомат с азартными играми в Японии'),
+('Ремонт', 'Как починка, только ремонт'),
+('Ну я не ебу, реально', 'Отвечаю')
+
+INSERT INTO Agreements
